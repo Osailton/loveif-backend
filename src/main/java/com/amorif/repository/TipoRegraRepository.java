3a2559a59c8e4239b696b6e528008bf55eebbad3
Manoel <manoelricardo38@gmail.com>
2024-10-31 13:29:16 -0300

    BE-003 cria repository para TipoRegra

---
+++ src/main/java/com/amorif/repository/TipoRegraRepository.java
@@ -0,0 +1,9 @@
+package com.amorif.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.amorif.entities.TipoRegra;
+
+public interface TipoRegraRepository extends JpaRepository<TipoRegra, Long> {
+
+}
